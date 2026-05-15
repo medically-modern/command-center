945de24cc8ffdbb86186768c59b61f8e2c24d0b8
Josh Hoffman <jhrunner1@gmail.com>
2026-05-15 14:56:07 +0000

    Revert "fix: resolve undefined PAGE in profile fetchItemAssets"
    This reverts commit ae663d1436dc4115f0d527a699bf2faa46eceb77.

--- src/lib/profile/mondayApi.ts
+++ src/lib/profile/mondayApi.ts
@@ -522,7 +522,7 @@ export async function fetchItemAssets(itemId: string): Promise<MondayAsset[]> {
   const query = `
     query ($boardId: ID!, $itemId: ID!) {
       boards(ids: [$boardId]) {
-        items_page(limit: 1, query_params: { ids: [$itemId] }) {
+        items_page(limit: ${PAGE}, query_params: { ids: [$itemId] }) {
           cursor
           items {
             assets(assets_source: all) { id name url public_url }
